fa44b26657f7fa12598d31ff2a8a895a689a27f8
JoaoNettoo <joao_neto100@hotmail.com>
2025-02-17 01:57:04 -0300

    Atualizações

--- static/js/main.js
+++ static/js/main.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location.href ="{% url 'food-menu' %}"; // Redireciona para cardapio se autenticado
       } else {
         alert("Faça login para acessar o cardápio.");
-        window.location.href ="{% url 'loginForm' %}"; // Redireciona para a seção de login
+        window.location.href ="{% url 'contact' %}"; // Redireciona para a seção de login
       }
     });
   }
